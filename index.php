f00acba72e4148d3501d28897cc0bc374542a0bf
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2017-05-26 08:45:00 +0200

    Added cron job: fetch news

--- index.php
+++ index.php
@@ -1,51 +1,7 @@
 <?php
-require('lib/simple_html_dom.php');
-
-function fetch($url)
-{
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => $url,
-        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0'
-        ));
-    $resp = curl_exec($curl);
-    curl_close($curl);
-    return $resp;
-}
-
-$main = fetch('http://m.radiogdansk.pl/autopilot');
-$matches = array();
-preg_match_all('/href="\/index\.php(\/relacje-live\/[^"]+)/', $main, $matches);
-$links = array_unique($matches[1]);
-$link = 'http://m.radiogdansk.pl' . $links[0];
-$html = str_get_html(fetch($link));
-
-$hours = array();
-$headers = array();
-$descriptions = array();
-$table = array();
-
-foreach ($html->find('.middle-date-hours') as $key => $hour) {
-    $hours[] = $hour->innertext;
-}
-
-foreach ($html->find('.catItemTitle-live') as $key => $header) {
-    $headers[] = trim($header->innertext);
-}
-
-foreach ($html->find('.latestItemIntroText') as $key => $description) {
-	$text = trim(strip_tags($description->innertext));
-	$text = preg_replace("/Podziel się/", "", $text);
-	$text = preg_replace("/\s+/", " ", $text);
-    $descriptions[] = trim($text);
-}
-
-for ($i = 0; $i < count($hours); $i++) {
-    $table[$i]['hour'] = $hours[$i];
-    $table[$i]['header'] = $headers[$i];
-    $table[$i]['description'] = $descriptions[$i];
-}
+$memcache = new Memcache();
+$news = $memcache->get('news');
+if (empty($news)) $news = array();
 
 header('Content-Type: application/json');
-echo json_encode($table);
+echo json_encode($news);
