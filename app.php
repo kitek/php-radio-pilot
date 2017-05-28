a059e91c78178f00b0165e10f2ceb90fed1d9119
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2017-05-28 11:05:52 +0200

    Added endpoints for users.

--- app.php
+++ app.php
@@ -4,10 +4,23 @@ use Silex\Application;
 
 $app = new Application();
 
-$app->get('/', function () use ($app) {
+$app->get('/news', function () use ($app) {
 	$memcache = new Memcache();
 	$news = $memcache->get('news') ?: [];
 	return $app->json($news);
+});
+
+$app->post('/users/register', function () use ($app) {
+	
+	// -> deviceToken
+	// <- secretToken
+	
+});
+
+$app->post('/users/unregister', function () use ($app) {
+
+	// -> deviceToken, secretToken
+
 });
 
 return $app;
