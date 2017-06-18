b8a788d3481256c7e80bd23507c03ead2f9b1e36
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2017-06-18 07:39:50 +0200

    JSON error handler.

--- src/app.php
+++ src/app.php
@@ -2,6 +2,7 @@
 
 use App\DataModel\User;
 use Silex\Application;
+use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Yaml\Yaml;
 
 $app = new Application();
@@ -10,5 +11,14 @@ $app['config'] = Yaml::parse(file_get_contents($config));
 $app['model.user'] = new User();
 $app['debug'] = strpos(getenv('SERVER_SOFTWARE'), 'Development') === 0;
 $app->register(new Silex\Provider\ServiceControllerServiceProvider());
+
+ErrorHandler::register();
+
+$app->error(function (\Exception $e) use ($app) {
+    return $app->json([
+        'error' => $e->getMessage(),
+        'code' => 500
+    ], 500);
+});
 
 return $app;
